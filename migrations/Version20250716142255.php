5f9d2eb2305bcca98a348f9e544f0ea46fb54e36
halat <111527681+Mhalat1@users.noreply.github.com>
2025-07-16 16:24:46 +0200

    versionSQLaPasserEnNoSQLpourRENDER

---
+++ migrations/Version20250716142255.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250716142255 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE session_module CHANGE institution_id institution_id INT NOT NULL');
+        $this->addSql('ALTER TABLE utilisateur ADD date_creation DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE utilisateur DROP date_creation');
+        $this->addSql('ALTER TABLE session_module CHANGE institution_id institution_id INT NOT NULL');
+    }
+}
